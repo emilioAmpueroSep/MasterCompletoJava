3f12e35b889b31f3b36057a7b15925c126d0d768
Emilio Ampuero <emilio.ampuero.s7@gmail.com>
2023-01-30 01:16:58 -0300

    casting and exercise ready

--- src/_BVariables/I_TypeConversion.java
+++ src/_BVariables/I_TypeConversion.java
@@ -3,9 +3,8 @@ package _BVariables;
 public class I_TypeConversion {
     public static void main(String[] args) {
 
-        /*
-        * we can convert to all type of data float, double, long , etc.
-        * */
+        System.out.println("======Convert variable type string to int ======");
+        // * we can convert to all type of data float, double, long , etc.
 
         String StrNumber = "50";
         
@@ -19,6 +18,58 @@ public class I_TypeConversion {
         String StrLogic = "true"; // TRUE, TruE all it's Ok
         boolean booleanLogic = Boolean.parseBoolean(StrLogic);
         System.out.println("booleanLogic = " + booleanLogic);
+
+
+        System.out.println("\n======Convert variable type int to string======");
+
+        int anotherIntNumber = 100;
+
+        System.out.println("anotherIntNumber = " + anotherIntNumber);
+
+        // wrapper class
+        String anotherStrNumber = Integer.toString(anotherIntNumber);
+        System.out.println("anotherStrNumber = " + anotherStrNumber);
+
+        anotherStrNumber = String.valueOf(anotherIntNumber+10);
+        System.out.println("anotherStrNumber = " + anotherStrNumber);
+//        it is to concat it, not sum literals:
+//        String anotherStrNumber2 = String.valueOf(StrNumber+10);
+//        System.out.println("anotherStrNumber2 = " + anotherStrNumber2);
+
+        double anotherDoubleReal = 1.23456;
+//        double anotherDoubleReal = 1.23456e2;
+        String anotherStrReal = Double.toString(anotherDoubleReal);
+        System.out.println("anotherStrReal = " + anotherStrReal);
+
+        anotherStrReal = String.valueOf(1.23456f);
+//        anotherDoubleReal = 1.23456e2f;
+        System.out.println("anotherStrReal = " + anotherStrReal);
+
+
+        System.out.println("\n====== (CASTING) Conversion among primitives data ======");
+
+        /*
+        we can cast types data, but we have to be careful with lost of data, in terms lack of space
+        in memory a type of data (ambiguous data). Recommend to change since int to long, char to short, etc. not int to short, long to int, short to char, etc.
+        */
+
+        // primitives can be change between other primitives data or floats data, but not to string or boolean.
+
+        int i = 22768;
+//        int i = 32768; // problems, because it will be ambiguous data
+        short s = (short) i;
+        System.out.println("s = " + s);
+        long l = i;
+        System.out.println("l = " + l);
+        System.out.println(Short.MAX_VALUE);
+        char b = (char) i;
+        System.out.println("b = " + b);
+        float f = (float) i;
+        System.out.println("f = " + f);
+
+
+
+
         
     }
 }
