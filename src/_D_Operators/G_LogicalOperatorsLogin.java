aa99d93622992cfcf7438b3d502d4aa93f1ec53c
Emilio Ampuero <emilio.ampuero.s7@gmail.com>
2023-01-31 23:30:21 -0300

    operators and its functions

---
+++ src/_D_Operators/G_LogicalOperatorsLogin.java
@@ -0,0 +1,57 @@
+package _D_Operators;
+
+import java.util.Scanner;
+
+public class G_LogicalOperatorsLogin {
+    public static void main(String[] args) {
+        /*
+
+        // Static array, it cant change capacity of literal you can add, three max in this case.
+        String[] usernames = new String[3]; // start 0 till 2
+        String[] passwords = new String[3];
+        usernames[0] = "andres";
+        passwords[0] = "12345";
+
+        usernames[1] = "admin";
+        passwords[1] = "12345";
+
+        usernames[2] = "pepe";
+        passwords[2] = "12345";
+
+        */
+        String[] usernames = {"andres", "admin", "pepe"};
+        String[] passwords = {"123", "1234", "12345"};
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el username");
+        String u = scanner.next();
+
+        System.out.println("Ingrese el password");
+        String p = scanner.next();
+
+        boolean esAutenticado = false;
+
+        for(int i = 0; i < usernames.length; i++){
+            esAutenticado = (usernames[i].equals(u) && passwords[i].equals(p))? true: esAutenticado;
+
+            /*
+            if( (usernames[i].equals(u) && passwords[i].equals(p)) ){
+                esAutenticado = true;
+                break;
+            }
+            */
+        }
+
+        String mensaje = esAutenticado ? "Bienvenido usuario ".concat(u).concat("!") :
+                "Username o contraseña incorrecto!\nLo sentimos, requiere autenticación";
+        System.out.println("mensaje = " + mensaje);
+
+        /*if(esAutenticado){
+            System.out.println("Bienvenido usuario ".concat(u).concat("!"));
+        } else {
+            System.out.println("Username o contraseña incorrecto!");
+            System.out.println("Los siento, requiere autenticación");
+        }*/
+    }
+}
