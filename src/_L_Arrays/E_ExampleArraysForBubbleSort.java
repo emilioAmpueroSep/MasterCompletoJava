11c957f917e58ee4e2c4320b005c495f3dad34d5
Emilio Ampuero <emilio.ampuero.s7@gmail.com>
2023-02-21 23:17:08 -0300

    folliwing arrays

--- src/_L_Arrays/E_ExampleArraysForBubbleSort.java
+++ src/_L_Arrays/E_ExampleArraysForBubbleSort.java
@@ -15,6 +15,24 @@ public class E_ExampleArraysForBubbleSort {
         }
     }
 
+    public static void sortBubble(Object[] arr) {
+        int total = arr.length;
+        int counter = 0;
+
+        for (int i = 0; i < total - 1; i++) {
+            for (int j = 0; j < total - 1 - i; j++) {
+//                doing autoboxing
+                if (((Comparable) arr[j + 1]).compareTo(arr[j]) > 0) {
+                    Object auxiliar = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = auxiliar;
+                }
+                counter++;
+            }
+        }
+        System.out.println("counter = " + counter);
+    }
+
     public static void main(String[] args) {
 
         String[] products = {"Kingston Pendrive 64GB", "Samsung Galaxy",
@@ -24,25 +42,26 @@ public class E_ExampleArraysForBubbleSort {
 
         int total = products.length;
 
-        int counter = 0;
-
-        for (int i = 0; i < total - 1; i++) {
-
-            for (int j = 0; j < total -1 -i; j++) {
-                // doing a sort manually:
-                if (products[i+1].compareTo(products[j]) < 0) {
-                    String auxiliar = products[j];
-                    products[i] = products[j+1];
-                    products[j] = auxiliar;
-                }
-                counter++;
-            }
-        }
-        System.out.println("counter = " + counter);
+        sortBubble(products);
 
         System.out.println("=== Using for ===");
         for (int i = 0; i < total; i++) {
             System.out.println("To index " + i + " : " + products[i]);
         }
+
+
+        Integer[] numeros = new Integer[4];
+
+        numeros[0] = 10;
+        numeros[1] = Integer.valueOf("7");
+        numeros[2] = (int) 35L;
+        numeros[3] = -1;
+
+        sortBubble(numeros);
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("i = " + i + " : " + numeros[i]);
+        }
+
     }
 }
