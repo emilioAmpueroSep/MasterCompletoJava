ae91ce986913a78f5c661941b4c39bf16900bcd9
Emilio Ampuero <emilio.ampuero.s7@gmail.com>
2023-02-26 22:39:47 -0300

    learning about POO

--- src/D_Poo/Automobile.java
+++ src/D_Poo/Automobile.java
@@ -1,12 +1,83 @@
 package D_Poo;
 
 public class Automobile { // template
-    String brand;
-    String model;
-    String color = "silver";
-    double displacement;
-    int tankCapacity = 40;
 
+    // 1. Attributes:
+    private String brand;
+    private String model;
+    private String color = "silver";
+    private double displacement;
+    private int tankCapacity = 40;
+
+    public Automobile() {
+
+    }
+
+    // 2. Constructors:
+    public Automobile(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public Automobile(String brand, String model, String color) {
+        this(brand, model); // A way to bring attributes of other constructor similar. to coding less lines.
+        this.color = color;
+    }
+
+    public Automobile(String brand, String model, String color, double displacement) {
+        this(brand, model, color); // A way to bring attributes of other constructor similar. to coding less lines.
+        this.displacement = displacement;
+    }
+
+    public Automobile(String brand, String model, String color, double displacement, int tankCapacity) {
+        this(brand, model, color, displacement); // A way to bring attributes of other constructor similar. to coding less lines.
+        this.tankCapacity = tankCapacity;
+    }
+
+
+    // 3. Setters and Getters:
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getDisplacement() {
+        return displacement;
+    }
+
+    public int getTankCapacity() {
+        return tankCapacity;
+    }
+
+    public void setBrand() {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDisplacement(double displacement) {
+        this.displacement = displacement;
+    }
+
+    public void setTankCapacity(int tankCapacity) {
+        this.tankCapacity = tankCapacity;
+    }
+
+
+    // 4. Operation methods(calculate, queries; etc.)
     public String seeDetail() {
 //        String model = "other";
         StringBuilder sb = new StringBuilder();
@@ -15,6 +86,7 @@ public class Automobile { // template
 //        sb.append("auto.model = " + model); // calling a local variable model
         sb.append("\nauto.color = " + this.color);
         sb.append("\nauto.displacement = " + this.displacement);
+        sb.append("\nauto.tankCapacity = " + this.tankCapacity);
         return sb.toString(); // return something
     }
 
@@ -42,18 +114,41 @@ public class Automobile { // template
     }
 
     public float calculateConsumptionFuel(int km, float percentageFuel) {
-        return km / (tankCapacity*percentageFuel);
+        return km / (tankCapacity * percentageFuel);
     }
 
     public float calculateConsumptionFuel(int km, int percentageFuel) {
-        return km / (tankCapacity*(percentageFuel/100f));
+        return km / (tankCapacity * (percentageFuel / 100f));
     }
 
+// 5. Fix or overide method:
 
+    // this notation is not obligatory; here we are over writting method which is located in object class(father):
+    @Override // changing the behavior of method to comparate arguments, not the objects. (polymorphism)
+    public boolean equals(Object obj) {
+        if (this == obj) { // comparing for reference(both are the same)
+            return true;
+        }
+        if (!(obj instanceof Automobile)) { // instanceof, compare if obj it's or not of Automobile type.
+            return false;
+        }
+        Automobile a = (Automobile) obj; // casting obj to Automobile class
+        return (this.brand != null && this.model != null
+                && this.brand.equals(a.getBrand())
+                && this.model.equals(a.getModel()));
+    }
 }
 
+/*
+ - class entity; entity = (characteristic + behavior).
+ - classes cannot be private because it cannot be invoked in other part of the program.
+ - class accepts encapsulation(visibility) of type public or default only.
+ - it is not a good practice to use SOUT in classes of entity like this one, it is better to use it in main class(return a var only).
+ - It's a good practice private or protected variables or attributes.
+ - getter and setter is a standart to read and modify attributes (hidden) and both words must be used in the methods.
+ - constructor methods is a method type void but that word is no used on it. it's only used for inicialiting an object with data or
+ without data (it can exist more than 1 constructor method). And it cannot be invoked like a common metohd.
+ - All objects are differents, even though they have the same arguments added in its parameters.
+ - We can over write methods that exists by default to change its behavior, we have to add notation @override
 
-// class entity; entity = (characteristic + behavior).
-// classes cannot be private because it cannot invoke in other part of the program.
-// class accepts encapsulation of public or default only.
-// it is not a good practice to use SOUT in classes of entity like this one, it is better to use it in main class(return a var only).
+ */
--- src/D_Poo/ExampleAutomobile.java
+++ src/D_Poo/ExampleAutomobile.java
@@ -1,10 +1,19 @@
 package D_Poo;
 
+import java.util.Date;
+
 public class ExampleAutomobile {
     public static void main(String[] args) {
 
-        Automobile auto = new Automobile(); // we instance the object Automobile and we save in the memory of program the referece towards this object
+        Automobile auto = new Automobile("Subaru", "Impreza"); // we instance the object Automobile and we save in the memory of program the reference towards this object.
+        Automobile auto2 = new Automobile("Mazda", "323", "Red", 1.8); // instantiating a new object called auto2
+        Automobile auto3 = new Automobile("Nissan", "Navara", "Pink", 1.5, 50);
+        Automobile auto4 = new Automobile("Nissan", "Navara", "Pink", 1.5, 50);
+        Automobile auto5 = new Automobile();
+        Date date = new Date();
+/*
 
+// Not hidden:
         auto.brand = "Subaru";
         auto.model = "Impreza";
         auto.color = "black";
@@ -18,8 +27,28 @@ public class ExampleAutomobile {
         auto2.displacement = 1.8;
 //        System.out.println("auto2.brand = " + auto2.brand); System.out.println("auto2.model = " + auto2.model); System.out.println("auto2.color = " + auto2.color); System.out.println("auto2.displacement = " + auto2.displacement);
 
+ */
+// Hidden:
+//        auto.setBrand("Subaru");
+//        auto.setModel("Impreza");
+//        auto.setColor("black");
+        auto.setDisplacement(2.0);
+        auto.setTankCapacity(30);
+//        System.out.println("auto2.brand = " + auto.getBrand());
+
+//        auto2.setBrand("Mazda");
+//        auto2.setModel("323");
+//        auto2.setColor("white");
+//        auto2.setDisplacement(1.8);
+//        System.out.println("auto2.brand = " + auto2.getBrand());
+
+        System.out.println("Are they equal? " + (auto3 == auto4));
+        System.out.println("Are they equal? " + (auto3.equals(auto4)));
+        System.out.println("Are they equal? " + (auto3.equals(auto5)));
+        System.out.println("Are they equal? " + (auto3.equals(date)));
         System.out.println(auto.seeDetail());
         System.out.println(auto2.seeDetail());
+        System.out.println(auto3.seeDetail());
         System.out.println(auto.speedUp(3000));
         System.out.println(auto.brake());
 
@@ -27,6 +56,7 @@ public class ExampleAutomobile {
 
         System.out.println("km per liters " + auto.calculateConsumptionFuel(300, 0.6f));
         System.out.println("km per liters " + auto.calculateConsumptionFuel(300, 60));
+        System.out.println("km per liters " + auto3.calculateConsumptionFuel(300, 60));
 
     }
 }
